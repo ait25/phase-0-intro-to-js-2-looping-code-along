3c7904e927d197d812aec8c0bab3685f2cc922e6
Alejandro Fernandez <fernandezale007@gmail.com>
2023-04-07 09:21:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
+
